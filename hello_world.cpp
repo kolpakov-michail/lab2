4f233011063fdd9f457b36aed80273cf05b513b7
mike <kolpmikel@mail.ru>
2022-04-03 11:58:44 +0300

    -style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main(){
-	string name; // объявляем переменную name
-	std::cin >> name;
-	std::cout << "Hello, world, from " << name << endl;
-	return 0;
+int
+main()
+{
+  string name; // объявляем переменную name
+  std::cin >> name;
+  std::cout << "Hello, world, from " << name << endl;
+  return 0;
 }
